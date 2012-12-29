6e9197e265e6fcc4dc54bbb4a3e467b48bde6ffe
yeray <yerayrm90@gmail.com>
2012-12-29 19:49:43 +0000

    Practica finalizada

--- toopaste.rb
+++ toopaste.rb
@@ -4,13 +4,13 @@ require 'syntaxi'
 
 class String
   def formatted_body(lang)
-    styles = {:ruby => "syntax_ruby"}
-    source = "[code lang='ruby']
+    types = {:ruby => "syntax_ruby", :xml => "syntax_xml", :yaml => "syntax_yaml"}
+    source = "[code lang='#{lang}']
                 #{self}
               [/code]"
     html = Syntaxi.new(source).process
     %Q{
-      <div class="syntax syntax_ruby">
+      <div class="syntax #{types[lang.to_sym]}">
         #{html}
       </div>
     }
@@ -22,5 +22,5 @@ get '/' do
 end
 
 post '/' do
-	erb :showruby, :locals => {:code => params[:code], :type => params[:lang]}
+  erb :show, :locals => {:result => params[:body], :type => params[:lang]}
 end
